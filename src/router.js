68c14da37689af8ef50e895929d47602188ca07e
Hector Vasquez <hvasquezdev@gmail.com>
2019-03-22 21:09:27 -0400

    Disabled History mode in router

--- src/router.js
+++ src/router.js
@@ -8,7 +8,6 @@ const AppTrackDetail = () => import ('@/components/layout/AppTrackDetail.vue');
 Vue.use(Router);
 
 export default new Router({
-  mode: 'history',
   base: process.env.BASE_URL,
   routes: [
     {
